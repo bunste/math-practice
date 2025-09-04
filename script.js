c3721307d1f51d66ff9832a071c2bcb33a62752b
Stefan Bungter <stefan.bungter@pressrelations.de>
2025-09-04 21:48:20 +0200

    Generating exercises and checking answers

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  function newExercise() {
+    const a = Math.floor(Math.random() * (100 - 2 + 1)) + 2; // 2 bis 100 inkl.
+    const b = Math.floor(Math.random() * (a - 1)) + 1; // 1 <= b < a
+
+    // Insert to DOM
+    $('#numA').text(a);
+    $('#numB').text(b);
+
+    // Empty answer field
+    $('#answer').val('');
+  }
+
+  // Generate first exercise
+  newExercise();
+
+  // Click handlers
+
+  $('#nextExercise').click(function() {
+    newExercise();
+  });
+
+  $('#checkAnswer').click(function() {
+    const answer = Number($('#answer').val());
+    const a = Number($('#numA').text());
+    const b = Number($('#numB').text());
+    
+    if(answer === a - b) {
+      // Show confetti
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+      });
+      console.log('Richtig!');
+    } else {
+      console.log('Versuche es nochmal.');
+    }
+  });
+});
